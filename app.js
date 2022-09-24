c7214c13ead4f97035b93ca4d5e2ffda2af7819f
RuchaRK <ruchakathar@gmail.com>
2022-09-24 18:29:29 +0530

    making the css changes

--- app.js
+++ app.js
@@ -4,40 +4,61 @@ var currentAmount = document.querySelector("#current-amt")
 var checkBtn = document.querySelector("#check-btn")
 var output = document.querySelector("#output")
 var message = ""
+var flag = 0
 
 function calProfitAndLoss(initial,quantity,current)
 {
+    console.log(typeof(initial))
+    console.log(quantity)
+    console.log(current)
 
-    if( initial < current)
+    if(initial < current)
     {
+        console.log("in first if")
         var profit = (current - initial)*quantity;
         var profitPercent = (profit/current)*100
-        message = "Hey there is loss of" + profit + " and a loss percent of " + profitPercent
-        showMessage(message)
-        
-    }
+        var flag = 0
+        message = "Hey there is profit of " + profit.toFixed(2) + " and a profit percent of " + profitPercent.toFixed(2)
+        showMessage(message,flag)   
+    }   
     else if(initial > current)
     {
         var loss = (initial - current) * quantity
         var lossPercent = (loss/initial)*100
-        message = "Hey there is loss of" + loss +" and a loss percent of " + lossPercent
-        showMessage(message)
+        var flag = 1
+        message = "Hey there is loss of " + loss.toFixed(2) + " and a loss percent of " + lossPercent.toFixed(2)
+        showMessage(message,flag)
     }
     else{
+        var flag = 2
         message = "This is neither Loss or Profit"
-        showMessage(message)
+        showMessage(message,flag)
     }
 
 }
 
-function showMessage(message)
+function showMessage(message,flag)
 {
+    if(flag === 0 ){
     output.innerText = message
+    output.style.color = "#00B1D2"
+}
+    else if (flag === 1)
+    {
+        output.innerText = message
+        output.style.color = "red"
+        
+    }
+    else{
+        output.innerText = message
+        output.style.color = "purple"
+
+    }
 }
 
 function clickHandler()
 {
-  calProfitAndLoss(initialAmount.value,quantity.value,currentAmount.value)
+  calProfitAndLoss(Number(initialAmount.value),Number(quantity.value),Number(currentAmount.value))
 }
 
 checkBtn.addEventListener('click',clickHandler)
