7f30a65e636dc381cd1ebb6e18f47b79b5288ad9
RuchaRK <ruchakathar@gmail.com>
2022-10-04 21:28:04 +0530

    Checking for negative values

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ function calProfitAndLoss(initial,quantity,current)
     {
         console.log("in first if")
         var profit = (current - initial)*quantity;
-        var profitPercent = (profit/current)*100
+        var profitPercent = (profit/(initial*quantity))*100
         var flag = 0
-        message = "Hey there is profit of " + profit.toFixed(2) + " and a profit percent of " + profitPercent.toFixed(2)
+        message = "Hey there is profit of " + profit.toFixed(2) + " and a profit percent of " + profitPercent.toFixed(2) + "%"
         showMessage(message,flag)   
     }   
     else if(initial > current)
     {
         var loss = (initial - current) * quantity
-        var lossPercent = (loss/initial)*100
+        var lossPercent = (loss/(initial*quantity))*100
         var flag = 1
-        message = "Hey there is loss of " + loss.toFixed(2) + " and a loss percent of " + lossPercent.toFixed(2)
+        message = "Hey there is loss of " + loss.toFixed(2) + " and a loss percent of " + lossPercent.toFixed(2) + "%"
         showMessage(message,flag)
     }
     else{
@@ -56,9 +56,25 @@ function showMessage(message,flag)
     }
 }
 
+
 function clickHandler()
-{
-  calProfitAndLoss(Number(initialAmount.value),Number(quantity.value),Number(currentAmount.value))
+{ 
+    var iniAmt = Number(initialAmount.value)
+    var stockQuantity = Number(quantity.value)
+    var currAmount =  Number(currentAmount.value)
+    console.log(iniAmt)
+    console.log(Math.sign(iniAmt))
+
+    if(Math.sign(iniAmt) === -1 || Math.sign(stockQuantity) === -1 || Math.sign(currAmount) === -1)
+    {
+        output.innerText = "Please enter value greater than or eqaual to zero."
+        output.style.color = "green"
+    }
+    else{
+        calProfitAndLoss(iniAmt,stockQuantity,currAmount)
+
+    }
+
 }
 
 checkBtn.addEventListener('click',clickHandler)
